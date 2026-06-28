12766c3d8d48c8fa668e59093faca0cb330bf8ce
KutzWilliam <kutz528@gmail.com>
2026-06-28 13:46:02 -0300

    fix secure git

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,8 @@ import app from './app';
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`🚀 Servidor do Guardião a correr na porta ${PORT}`);
+app.listen(PORT as number, '0.0.0.0', () => {
+  console.log(`🚀 Servidor do Guardião rodando na porta ${PORT}`);
 });
 
 export default app;
